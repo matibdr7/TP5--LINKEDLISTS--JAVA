7638b79a6f9b20cb524b8cb7c8ce295771333930
@matibdr7 <matias.bdr7@gmail.com>
2024-11-03 01:03:56 -0300

    Implemento clase SimpleLinkedList + interface ILinkedList

---
+++ src/Punto1/App.java
@@ -0,0 +1,13 @@
+/*
+Implementar una clase “ListaEnlazadaSimple” que permita insertar, eliminar, buscar y mostrar elementos. 
+Comprobar el correcto funcionamiento de  la implementación propuesta creando una instancia de  la lista. Se deben guardar X cantidad de objetos de la clase Empleado, la cual posee un legajo, dni, nombre, fecha de  nacimiento  y  correo  electrónico.  Realizar  las  operaciones  a  pedido  del  usuario.  Para  ello,  se  deberá utilizar un menú de opciones.
+*/
+
+package Punto1;
+
+public class App {
+    public static void main(String[] args){
+
+    }
+}
+ 
